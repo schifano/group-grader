963729538e926c4cb31f78e8ae697c213cfd92ee
jdboom <jdboomg@ilstu.edu>
2013-11-13 20:53:42 -0600

    got delay to work
    run it and check it out. PAY VERY CLOSE attention to the console, and
    sing along if you want.

--- GradeApp.java
+++ GradeApp.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class GradeApp extends Thread
 {
-	private Course processingCourse;
+	
 
 	/**
 	 * @param args
@@ -12,37 +12,34 @@ public class GradeApp extends Thread
 	 */
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		Course processingCourse;
 		Thread delay = new Thread();
         delay.start();
 
 		MainGUIScreen screen = new MainGUIScreen();
-		synchronized(delay)
+		while(screen.getXMLFile() == null)
 		{
-			try
-			{
-				delay.wait();
-			}catch(InterruptedException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		{
-			FileInputStream fin = null;
-
+			System.out.println("Sitting on the dock of the bay");
+			System.out.println("watching the CPU cycles roll away, oooh");
 			
+		}
+		System.out.println("it worked");
+
+			FileInputStream fin = null;
 			try 
 			{
+				System.out.println("Check 4");
 				fin = new FileInputStream(screen.getXMLFile());
+				System.out.println("Check 5");
 				processingCourse = CourseXMLParser.readCourse(fin);
+				System.out.println("Check 6");
+				System.out.println(processingCourse);
 			} 
 			catch (FileNotFoundException e) 
 			{
 				// TODO Auto-generated catch block
-				System.out.println("File not f)ound")
+				System.out.println("File not found");
 			}
 			
-			System.out.println(course);
-		}
-
 	}
 }
